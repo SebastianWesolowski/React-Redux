d5cafb962e3e9705d4e88e3e324ae07a46da8ccd
SebastianWesolowskiBpol <sebastian.weoslowski@bpol.net>
2021-01-03 20:46:33 +0100

    Add initial store

---
+++ src/store/store.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const initialState = {
+  cart:[]
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return{
+        ...state, cart: [...state.cart, action.item]
+      }
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware()));
